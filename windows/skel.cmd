d3fdbbcd1cee0a4395cf732b9dc94353dc6440c5
Donald Raikes <don.raikes@nyu.edu>
2014-08-07 16:45:37 -0700

    Added windows command-script skelleton

---
+++ windows/skel.cmd
@@ -0,0 +1,11 @@
+@echo off
+::------------------------------------------------------------
+::	File:
+::	Author:	Donald Raikes <don.raikes@nyu.edu>
+::	Date:
+::
+::------------------------------------------------------------
+SETLOCAL ENABLEEXTENSIONS
+
+:main
+	GOTO EOF
