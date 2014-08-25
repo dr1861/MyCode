d03fa78bbffbee01583edc2acdb35be3daebcd39
root <root@localhost.localdomain>
2014-08-24 20:27:58 -0700

    Updating dpkt1.py to remove non-printable characters from tcp.data

--- python/dpkt1.py
+++ python/dpkt1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import dpkt
+import dpkt, string
 
 f = open('vscommand.cap')
 pcap = dpkt.pcap.Reader(f)
@@ -15,11 +15,12 @@ for ts, buf in pcap:
     if ip.p==dpkt.ip.IP_PROTO_TCP: #Check for TCP packets
            tcp=ip.data 
            #ADD TCP packets Analysis code here
-           if tcp.dport == 9999:
-		       print str(tcp.data)
-           elif tcp.sport == 9999:
+           payload = ''.join(filter(lambda x:x in string.printable, tcp.data))
 
-		       print str(tcp.data)
+           if tcp.dport==9999:
+		       print payload.strip()
+           elif tcp.sport == 9999:
+		       print payload.strip()
 
 
 f.close()
